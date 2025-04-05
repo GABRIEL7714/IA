8cefbd41f527a8ff402382442173777bc6022041
Jean Marc Nadeau Huamani <101827583+Jean-Marc-NH@users.noreply.github.com>
2025-04-04 19:11:34 -0500

    Create Coloracion.cpp

---
+++ Coloracion.cpp
@@ -0,0 +1,68 @@
+#include <GL/glut.h>
+#include <vector>
+#include <iostream>
+#include <utility>
+#include <cmath>
+
+
+using namespace std;
+
+vector<pair<int,int>> puntos;
+vector<pair<int,int>> unir;
+
+void mouseCallBack(int,int,int,int);
+
+void reshape_cb (int w, int h) {
+	if (w==0||h==0) return;
+	glViewport(0,0,w,h);
+	glMatrixMode (GL_PROJECTION);
+	glLoadIdentity ();
+	gluOrtho2D(0,w,0,h);
+	glMatrixMode (GL_MODELVIEW);
+	glLoadIdentity ();
+}
+
+void display_cb() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1,0,0); 
+	glPointSize(30.0);
+	glBegin(GL_POINTS);
+	for(auto &p : puntos)glVertex2i(p.first, p.second);
+	glEnd();
+
+	glutSwapBuffers();
+}
+
+void initialize() {
+	glutInitDisplayMode (GLUT_RGBA|GLUT_DOUBLE);
+	glutInitWindowSize (640,480);
+	glutInitWindowPosition (100,100);
+	glutCreateWindow ("Ventana OpenGL");
+	glutDisplayFunc (display_cb);
+	glutReshapeFunc (reshape_cb);
+	glClearColor(1.f,1.f,1.f,1.f);
+	
+	glutMouseFunc(mouseCallBack);
+}
+
+int main (int argc, char **argv) {
+	glutInit (&argc, argv);
+	initialize();
+	glutMainLoop();
+	return 0;
+}
+
+void mouseCallBack(int button, int state, int x, int y)
+{
+	if(state == GLUT_DOWN)
+	{
+		pair<int,int> coord;
+		coord.first=x;
+		coord.second =abs(y-480);
+		puntos.push_back(coord);
+		cout << x << " , " << y  << "\n";
+	}
+	
+	
+}
+
